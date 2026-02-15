8e957ccece4769749f9fa81111f7e469000bc4a5
Tony Toscano <tonymichael16@gmail.com>
2026-02-15 17:22:58 -0500

    spatie-permissions: Added back a method that was removed accidently, added missing migration table

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -37,6 +37,13 @@ class UserFactory extends Factory
         ];
     }
 
+    public function unverified(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'email_verified_at' => null,
+        ]);
+    }
+
     /**
      * Indicate that the model has two-factor authentication configured.
      */
